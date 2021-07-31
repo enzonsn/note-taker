8011c60587e7ca474735daf0e029d0bf1263d1f9
enzonsn <enzonsperduti@gmail.com>
2021-07-30 22:56:35 -0500

    fixed api routing and added screenshot

--- routes/apir.js
+++ routes/apir.js
@@ -1,33 +1,23 @@
 const router = require('express').Router();
-const db = require('../')
+const {addNewNote, deleteNote} = require('../lib/notes');
+const notes = require('../db/db.json');
 
-fs.readFile(path.join(__dirname, '../db/db.json'), "utf8", (err, data)=>{
-    if (err) throw err
 
-    const note = JSON.parse(data);
+router.get('/notes', (req, res) => {res.send(notes);});
 
-    router.get('/notes', (req, res) => {  });
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    const newNote = addNewNote(req.body, notes);
+    res.json(newNote);
+});
 
-    router.post('/notes', (req, res) => {
-        let newBody = req.body;
-        console.log(note);
-        note.push(newBody);
-        console.log(note);
-        update(note);
-    });
+router.get('/notes/:id', (req, res) =>{
+    res.send(notes[req.params.id]);
+});
 
-    router.get('/notes/:id', (req, res) =>{
-        res.send(note[req.params.id]);
-        console.log("tried to touch note");
-    });
-
-    router.delete('/notes/:id', (req, res) => {
-        console.log(note);
-        note.splice(req.params.id, 1);
-        console.log(note);
-        update(note);
-    });
-
+router.delete('/notes/:id', (req, res) => {
+    deleteNote(req.params.id, notes);
+    res.json(true);
 });
 
 module.exports = router;
